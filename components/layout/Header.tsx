e72204835938d2097ceee06e9c39f036a44b3f59
JeongjooLee <jjlee1623@gmail.com>
2023-04-06 02:03:53 +0900

    feat : 헤더 로고 변경

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -11,21 +11,28 @@ export function Header() {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
-              viewBox="0 0 24 24"
+              className="w-10 h-10 text-white p-2 bg-orange-400 rounded-full"
             >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+              />
             </svg>
-            <span className="ml-3 text-xl">joometa portfolio</span>
+            <span className="ml-3 text-xl">joometa</span>
           </span>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/">
             <span className="mr-5 hover:text-gray-900">Home</span>
+          </Link>
+          <Link href="/skill">
+            <span className="mr-5 hover:text-gray-900">Skill</span>
           </Link>
           <Link href="/projects">
             <span className="mr-5 hover:text-gray-900">Projects</span>
